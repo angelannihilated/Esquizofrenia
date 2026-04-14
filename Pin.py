ebe66a012546abd61b67595ad811d6544f6b481e
Renatozilla <renato.nanculeo@inacapmail.cl>
2026-04-14 11:27:56 -0400

    LO ARREGLE
    Cachai que puse puras tonteras y puse mal una carpeta, pero ya lo arregle

---
+++ Pin.py
@@ -0,0 +1,14 @@
+
+
+print("Wena hermanito como tai")
+
+print("Loco hiciste un cambio o no en el prgrama?")
+
+print("Naaaah de pelos, quedo brutal")
+
+print("Pero cuentame po loco, que cambios has hecho?")
+
+print("Increible")
+
+print("Manda una foto de tu cara bro")
+
